24041acee7da2ad97b1ed3d6aae3e13a11e9bd67
Bryan Carlson <bryan.r.carlson@gmail.com>
2017-06-27 11:47:39 -0700

    Turns out I do need this, finished class

--- Measure/Models/Measurement.cs
+++ Measure/Models/Measurement.cs
@@ -6,6 +6,11 @@ namespace Nsar.Common.Measure.Models
 {
     public class Measurement
     {
+        public string Name { get; set; }
+        public DateTime DateTime { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
 
+        public PhysicalQuantity PhysicalQuantity { get; set; }
     }
 }
