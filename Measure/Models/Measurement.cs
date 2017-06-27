acd8243a07b877c1cc776effbacf5bb7009e2396
Bryan Carlson <bryan.r.carlson@gmail.com>
2017-06-27 11:34:38 -0700

    Half-heartidly created a new project to maybe be a nuget package.  Abandoned because I realized I dont need it right now

---
+++ Measure/Models/Measurement.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nsar.Common.Measure.Models
+{
+    public class Measurement
+    {
+
+    }
+}
---
+++ Measure/Models/PhysicalQuantity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nsar.Common.Measure.Models
+{ //TODO: this whole class!  Do it!
+    public class PhysicalQuantity
+    {
+        public double Value { get; set; }
+        public string Unit { get; set; }
+    }
+}
