6929c154633f847ae717eff60f6c99341a1740ac
Bryan Carlson <bryan.r.carlson@gmail.com>
2017-06-27 20:06:01 -0700

    Updated Measure

--- Measure/Models/Measurement.cs
+++ Measure/Models/Measurement.cs
@@ -6,11 +6,48 @@ namespace Nsar.Common.Measure.Models
 {
     public class Measurement
     {
-        public string Name { get; set; }
-        public DateTime DateTime { get; set; }
-        public double Latitude { get; set; }
-        public double Longitude { get; set; }
+        private readonly string name;
+        private readonly DateTime dateTime;
+        private readonly double latitude;
+        private readonly double longitude;
+        private readonly PhysicalQuantity physicalQuantity;
 
-        public PhysicalQuantity PhysicalQuantity { get; set; }
+        public string Name
+        {
+            get { return name; }
+        }
+        public DateTime DateTime
+        {
+            get { return dateTime; }
+        }
+        public double Latitude
+        {
+            get { return latitude; }
+        }
+        public double Longitude
+        {
+            get { return longitude; }
+        }
+
+        public PhysicalQuantity PhysicalQuantity
+        {
+            get { return physicalQuantity; }
+        }
+
+        public Measurement(
+            string name,
+            DateTime dateTime,
+            double latitude,
+            double longitude,
+            PhysicalQuantity physicalQuantity)
+        {
+            //TODO: Error checking
+
+            this.name = name;
+            this.dateTime = dateTime;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.physicalQuantity = physicalQuantity;
+        }
     }
 }
--- Measure/Models/PhysicalQuantity.cs
+++ Measure/Models/PhysicalQuantity.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace Nsar.Common.Measure.Models
-{ //TODO: this whole class!  Do it!
+{
     public class PhysicalQuantity
     {
-        public double Value { get; set; }
-        public string Unit { get; set; }
+        private readonly double value;
+        private readonly string unit;
+
+        public double Value { get { return value; } }
+        public string Unit { get { return unit; } }
+
+        public PhysicalQuantity(
+            double value,
+            string unit)
+        {
+            this.value = value;
+            this.unit = unit;
+        }
     }
 }
